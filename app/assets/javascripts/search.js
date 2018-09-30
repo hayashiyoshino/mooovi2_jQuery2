75ae18b53bdbd97f3f60f222e1ac94cd549e2435
林佳志乃 <twinklestaryoshino@gmail.com>
2018-09-30 15:51:21 +0900

    definition ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,6 +3,16 @@ $(function() {
     // テキストフィールドにキーが入力されたら関数実行
     var input = $(".search__query").val();
     // テキストフィールドに入力された値を取得し、inputという変数に代入
-    console.log(input);
+    $.ajax({
+      // ajax通信を行う
+      type: 'GET',
+      // HTTPメソッドはGET
+      url: '/products/search',
+      // 送り先のurlは/products/search
+      data: { keyword: input },
+      // 送るデータは{ keyword: input }
+      dataType: 'json'
+      // サーバーから返す値はjson形式
+    })
   });
 });
